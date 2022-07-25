d8b8af3177aa4f60ea4fa0a63a3c9cbb5c1bc5f6
Cass Wilkinson Saldana <cass.z.ws@gmail.com>
2022-07-25 11:47:58 -0400

    Add documentation at function and package level

--- R/matches.R
+++ R/matches.R
@@ -1,3 +1,14 @@
+#' Makes a standard dataframe for league data
+#'
+#' @param data_engsoc dataframe from engsoccerdata package
+#' @param country a country (one of Spain, Italy, Germany, England, Holland)
+#'
+#' @return a tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#'     `visitor`, etc.
+#' @export
+#'
+#' @examples
+#' uss_make_matches(engsoccerdata::spain, "Spain")
 uss_make_matches <- function(data_engsoc, country) {
   result <-
     data_engsoc |>
